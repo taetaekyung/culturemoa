0a0bb0e25aeb3963d96737ee433c38c9b6024b75
taetaekyung <taekyung.y123@gmail.com>
2023-07-26 14:23:15 +0900

    MainController 수정 [양태경]

--- dbja3/src/main/java/com/example/demo/controller/MainController.java
+++ dbja3/src/main/java/com/example/demo/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	@GetMapping("/")
-	public void main() {
-		
+	public String main() {
+		return "main";
 	}
 }
