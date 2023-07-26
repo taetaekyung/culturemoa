96efefd50cd8c4438adbdb7b24807f7b4da1a0d6
s0nnyday <lijnar100@naver.com>
2023-07-26 17:51:21 +0900

    행사리스트,디테일 수정중[손지민]

--- dbja3/src/main/java/com/example/demo/controller/EventController.java
+++ dbja3/src/main/java/com/example/demo/controller/EventController.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.demo.dao.EventDAO_jpa;
 import com.example.demo.dao.EventDAO_mb;
+import com.example.demo.entity.Event;
 
 import lombok.Setter;
 
@@ -14,14 +26,32 @@ import lombok.Setter;
 public class EventController {
 	
 	@Autowired
-	private EventDAO_jpa dao_jpa;
+	private EventDAO_jpa eventdao_jpa;
 	@Autowired
-	private EventDAO_mb dao_mb;
+	private EventDAO_mb eventdao_mb;
 	
 	@GetMapping("/event/eventlist")
-	public void eventList() {
+	public void eventList(Model model, @RequestParam(defaultValue = "0") int page) {
+	    int pageSize = 16; // 한 페이지에 보여줄 개수 (4열 4행이므로 16개씩)
+	    Pageable pageable = PageRequest.of(page, pageSize, Sort.by("eventno").descending());
+	    Page<Event> eventPage = eventdao_jpa.findAll(pageable);
+	    List<List<Event>> rows = new ArrayList<>();
+	    List<Event> currentRow = null;
+
+	    for (Event event : eventPage.getContent()) {
+	        if (currentRow == null || currentRow.size() >= 4) {
+	            currentRow = new ArrayList<>();
+	            rows.add(currentRow);
+	        }
+	        currentRow.add(event);
+	    }
+
+	    model.addAttribute("rows", rows);
+	    model.addAttribute("currentPage", page);
+	    model.addAttribute("totalPages", eventPage.getTotalPages());
 	}
-	   
+	
+	//행사 상세로 이동
 	@GetMapping("/event/eventdetail")
 	public void eventDetail() {
 	}
