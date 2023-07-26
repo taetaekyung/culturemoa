75fef5fa13862b70fbb6ab1dbdd934424a338cda
taetaekyung <taekyung.y123@gmail.com>
2023-07-26 14:17:29 +0900

    html, css, 이미지 추가 + pom.xml 수정 [양태경]

--- dbja3/src/main/java/com/example/demo/controller/BoardController.java
+++ dbja3/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-
+	
+	@GetMapping("/boards/board/freelist")
+	public void freelist() {
+		
+	}
 }
