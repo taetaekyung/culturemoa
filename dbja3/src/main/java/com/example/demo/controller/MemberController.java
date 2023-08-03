a89c112b247aa19c5ede92ba27ce892473951f58
taetaekyung <taekyung.y123@gmail.com>
2023-08-03 12:12:17 +0900

    마이페이지 비밀번호 수정 구현 완료 [양태경]

--- dbja3/src/main/java/com/example/demo/controller/MemberController.java
+++ dbja3/src/main/java/com/example/demo/controller/MemberController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.MemberDAO_jpa;
@@ -34,7 +38,31 @@ public class MemberController {
 	
 	
 	@GetMapping("/member/changepwd")
-	public void changepwd() {
+	public void changepwdform() {
+	}
+	
+	@RequestMapping(value="/member/changepwd", method = { RequestMethod.POST })
+	@ResponseBody
+	public Object changepwd(HttpSession session,@RequestParam("pwd")  String pwd,@RequestParam("newpwd") String newpwd,
+			@RequestParam("chkpwd") String chkpwd) {
+		Member m = (Member) session.getAttribute("m");
+		String userpwd = m.getPwd();
+		String msg = "";
+		if(pwd.equals(userpwd)) {
+			if(newpwd.equals(chkpwd)) {
+				msg = "비밀번호 변경이 완료되었습니다.";
+				m.setPwd(newpwd);
+				memberdao_jpa.save(m);
+			}
+			else {
+				msg = "새 비밀번호가 일치하지 않습니다.";
+			}
+		}
+		else {
+			msg = "현재 비밀번호와 일치하지 않습니다.";
+		}
+		
+		return msg;
 	}
 	
 
