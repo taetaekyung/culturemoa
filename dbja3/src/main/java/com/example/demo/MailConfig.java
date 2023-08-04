71fe21fada57619b52248644e38554121662c316
taetaekyung <taekyung.y123@gmail.com>
2023-08-04 09:36:47 +0900

    비밀번호 수정 및 메일 인증 추가 (메일 인증이 안된채로 수정 버튼 클릭 시 alert 뜨도록 수정 중) [양태경]

---
+++ dbja3/src/main/java/com/example/demo/MailConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+	@Bean
+	public JavaMailSenderImpl javaMailSender() {
+		JavaMailSenderImpl r = new JavaMailSenderImpl();
+		r.setHost("smtp.gmail.com");
+		r.setPort(587);
+		r.setUsername("green3potato@gmail.com");	// gmail계정을 설정합니다.
+		r.setPassword("ilxxccgjvfvsncbv");  // <--- 발행된 암호를 여기에 넣어 주세요.
+		r.setDefaultEncoding("UTF-8");
+		
+		Properties prop = new Properties();
+		prop.put("mail.smtp.starttls.enable", true);
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.ssl.checkserveridentity", true);
+		prop.put("mail.smtp.ssl.trust","*");
+		prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+		
+		r.setJavaMailProperties(prop);
+		return r;
+	}
+	
+}
