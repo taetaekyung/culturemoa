da39d975ba2623167d3d257f6fc64bc7900cf4ca
s0nnyday <lijnar100@naver.com>
2023-08-11 17:11:46 +0900

    로그인 관련 html/css 수정중[손지민]

--- dbja3/src/main/java/com/example/demo/controller/SignupController.java
+++ dbja3/src/main/java/com/example/demo/controller/SignupController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RegisterController {
-	
+public class SignupController {
 
 	@GetMapping("/signup")
 	public void signup() {
-		
 	}
 }
